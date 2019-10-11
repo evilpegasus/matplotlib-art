7e2d2deae99516fa5275c98bf33f305e5744ac7d
Ming Fong <mingfong07@hotmail.com>
2019-10-10 22:56:45 -0700

    timer and text

--- prime_plot.py
+++ prime_plot.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
- 
+import time
+
+start = time.time()
 p = []
-for n in range(1, 1000):
+max = 10000
+for n in range(1, max):
     print(n)
     root = int(np.around(np.sqrt(n)) + 0.5)
     for x in range(2, root):
@@ -10,13 +13,15 @@ for n in range(1, 1000):
             break
     else:
         p.append(n)
+end = time.time()
 print(p)
+print("Time elapsed:", end-start, "seconds")
+print(len(p), "prime numbers found between 0 and", max)
 
-
-fig = plt.figure()
+fig = plt.figure(facecolor='black')
 ax = fig.add_subplot(111, projection='polar')
 c = ax.scatter(p, p, c='darkturquoise', s=1)
 
 ax.axis('off')
 plt.show()
-plt.savefig('graph1.png', facecolor='black')
+#plt.savefig('graph1.png', facecolor='black')
