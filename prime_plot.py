e3c9847bd9f0e6bc01fe26eff772c02f335412d4
Ming Fong <mingfong07@hotmail.com>
2019-10-17 22:18:54 -0700

    asdf

--- prime_plot.py
+++ prime_plot.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+#Graphs primes up to max on a polar system with theta=prime and length=prime
+
 start = time.time()
 p = []
-max = 10000
+max = 100000
 for n in range(1, max):
     print(n)
     root = int(np.around(np.sqrt(n)) + 0.5)
@@ -20,8 +22,8 @@ print(len(p), "prime numbers found between 0 and", max)
 
 fig = plt.figure(facecolor='black')
 ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(p, p, c='darkturquoise', s=1)
-
+c = ax.scatter(p, p, c='darkturquoise', s=.1)
 ax.axis('off')
+
 plt.show()
-#plt.savefig('graph1.png', facecolor='black')
+fig.savefig('plot.png', dpi=400, facecolor=fig.get_facecolor(), edgecolor='none')
