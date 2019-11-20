0ccb7d0243a2e352754aa21f40d64fdfd84e21bd
Ming Fong <50010165+evilpegasus@users.noreply.github.com>
2019-11-20 07:35:32 -0800

    spam all the dir

--- spam.py
+++ spam.py
@@ -2,21 +2,41 @@
 
 import os
 
-max = 100
+max = 500
 i = 1
+j = 1
+file_path = "C:\\Users\\min.fon000\\Desktop\\"
+
 
 def spam():
     f = open("asdf" + str(i) + ".txt", "a")
     for x in range(2000):
         f.write("spam\n")
-    print("Created " + "asdf" + str(i) + ".txt")
+    print("Created " + os.getcwd() + "\asdf" + str(i) + ".txt")
     f.close()
+
+def spam_desktop():
+    f = open(file_path + "asdf" + str(i) + ".txt", "a")
+    for x in range(2000):
+        f.write("spam\n")
+    print("Created " + os.getcwd() + "\asdf" + str(i) + ".txt")
+    f.close()
+
+def unspam_desktop():
+    os.remove(file_path + "asdf" + str(i) + ".txt")
+    print("Deleted " + os.getcwd() + "\asdf" + str(i) + ".txt")
 
 def unspam():
     os.remove("asdf" + str(i) + ".txt")
-    print("Deleted " + "asdf" + str(i) + ".txt")
+    print("Deleted " + os.getcwd() + "\asdf" + str(i) + ".txt")
 
-while(i <= max):
-    spam()
-    unspam()
-    i += 1
+while(j <= 10):
+    while(i <= max):
+        #spam()
+        unspam()
+        #spam_desktop()
+        #unspam_desktop()
+        i += 1
+    os.chdir("..")
+    print("Changed directory to " + os.getcwd())
+    i = 1
